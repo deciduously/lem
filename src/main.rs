fa4e51aaa280475c7fcd5f41620698852fc30c2c
deciduously <bendlovy@gmail.com>
2017-02-28 09:24:23 -0500

    use Deref impl for clairty in db.rs

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,17 @@ mod schema;
 mod models;
 mod sysinfo;
 
-use db::DB;
+use db::Conn;
 use error::Error as ApiError;
 use rocket_contrib::JSON;
 use rocket::response::status::Created;
 use sysinfo::{create_entry, get_entries, get_entry, get_latest};
 
 #[get("/")]
-fn index(db: DB) -> Result<String, ApiError> {
+fn index(conn: Conn) -> Result<String, ApiError> {
     // TODO handle empty table case
-    let latest = get_latest(db.conn())?;
-    Ok(format!("Most recent record: #{}  - uname: {} uptime: {} into the table\nCreated @ {}\n",
+    let latest = get_latest(&conn)?;
+    Ok(format!("Most recent record: #{}  - uname: {} uptime: {} @ {}\n",
                latest.id,
                latest.uname,
                latest.uptime,
@@ -40,21 +40,21 @@ fn index(db: DB) -> Result<String, ApiError> {
 }
 
 #[get("/entries", format="application/json")]
-fn entries_get(db: DB) -> Result<JSON<Vec<SysInfo>>, ApiError> {
-    let entries = get_entries(db.conn())?;
+fn entries_get(conn: Conn) -> Result<JSON<Vec<SysInfo>>, ApiError> {
+    let entries = get_entries(&conn)?;
     Ok(JSON(entries))
 }
 
 #[get("/entry/<id>", format="application/json")]
-fn entry_get(db: DB, id: i32) -> Result<JSON<SysInfo>, ApiError> {
-    let entry = get_entry(db.conn(), id)?;
+fn entry_get(conn: Conn, id: i32) -> Result<JSON<SysInfo>, ApiError> {
+    let entry = get_entry(&conn, id)?;
     Ok(JSON(entry))
 }
 
 use self::models::{SysInfo, SysInfoData};
 #[post("/entries", format="application/json")]
-fn entry_create(db: DB) -> Result<Created<JSON<SysInfo>>, ApiError> {
-    let entry = create_entry(db.conn(), &SysInfoData::new())?;
+fn entry_create(conn: Conn) -> Result<Created<JSON<SysInfo>>, ApiError> {
+    let entry = create_entry(&conn, &SysInfoData::new())?;
     let url = format!("/entry/{}", entry.id);
     Ok(Created(url, Some(JSON(entry))))
 }
